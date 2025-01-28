09b7b96c9bb9d2d78c9d16c6aa1eaad9b8ac22c1
Aditya Chauhan <apple@50-0-0-58.static.sonic.net>
2025-01-28 12:32:23 +0530

    n

--- lnked_list_2.c
+++ lnked_list_2.c
@@ -1,30 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node {
-    int data;
-    struct Node* next;
+struct ListNode {
+    int val;
+    struct ListNode *next;
 };
 
-void insertAtBeginning(struct Node** head, int newData) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = newData;
-    newNode->next = *head;
-    *head = newNode;
+struct ListNode* merge(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode dummy;
+    struct ListNode* tail = &dummy;
+    dummy.next = NULL;
+
+    while (l1 && l2) {
+        if (l1->val < l2->val) {
+            tail->next = l1;
+            l1 = l1->next;
+        } else {
+            tail->next = l2;
+            l2 = l2->next;
+        }
+        tail = tail->next;
+    }
+
+    tail->next = l1 ? l1 : l2;
+    return dummy.next;
 }
 
-int main() {
-    struct Node* head = NULL;
-    insertAtBeginning(&head, 10);
-    insertAtBeginning(&head, 20);
-    insertAtBeginning(&head, 30);
-    printf("Linked List: ");
-    struct Node* temp = head;
-    while (temp != NULL) {
-        printf("%d -> ", temp->data);
-        temp = temp->next;
+struct ListNode* findMiddle(struct ListNode* head) {
+    struct ListNode* slow = head;
+    struct ListNode* fast = head;
+    struct ListNode* prev = NULL;
+
+    while (fast && fast->next) {
+        prev = slow;
+        slow = slow->next;
+        fast = fast->next->next;
     }
-    printf("NULL\n");
-    
-    return 0;
+
+    if (prev) prev->next = NULL;
+    return slow;
+}
+
+struct ListNode* sortList(struct ListNode* head) {
+    if (!head || !head->next) return head; 
+
+    struct ListNode* mid = findMiddle(head);
+    struct ListNode* left = sortList(head);
+    struct ListNode* right = sortList(mid);
+
+    return merge(left, right);
 }
